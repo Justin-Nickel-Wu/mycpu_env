1a2a7dc0708ccf43833c9a5f197a98560d383a3a
Justin-Nickel-Wu <1295360720@qq.com>
2025-01-27 15:33:43 +0800

    完成MEM_stage与WB_stage模块编写

--- myCPU/mycpu_top.v
+++ myCPU/mycpu_top.v
@@ -22,20 +22,6 @@ module mycpu_top(
 
 
 
-
-
-assign data_sram_en    = 1'b1;
-assign data_sram_we    = {4{mem_we && valid}};
-assign data_sram_addr  = alu_result;
-assign data_sram_wdata = rkd_value;
-
-assign mem_result   = data_sram_rdata;
-assign final_result = res_from_mem ? mem_result : alu_result;
-
-assign rf_we    = gr_we && valid;
-assign rf_waddr = dest;
-assign rf_wdata = final_result;
-
 // debug info generate
 assign debug_wb_pc       = pc;
 assign debug_wb_rf_we   = {4{rf_we}};
@@ -222,9 +208,11 @@ assign to_EX_data ={pc, //32
                     alu_op, //12
                     src1_is_pc, //1
                     src2_is_imm, //1
-
-
-}
+                    mem_we, //1
+                    res_from_mem, //1
+                    dest, //32
+                    gr_we //1
+                    };
 
 assign seq_pc       = pc + 3'h4;
 assign nextpc       = br_taken ? br_target : seq_pc;
@@ -365,6 +353,9 @@ wire [31:0] alu_src1   ;
 wire [31:0] alu_src2   ;
 wire [31:0] alu_result ;
 
+wire mem_we;
+wire res_from_mem;
+
 assign EX_ready_go = 1'b1;//无阻塞
 assign EX_allow_in = ~EX_valid | (EX_ready_go & MEM_allow_in);
 assign EX_to_MEM_valid = EX_valid & EX_ready_go;
@@ -382,7 +373,19 @@ assign {pc,
         imm,
         alu_op,
         src1_is_pc,
-        src2_is_imm} = to_EX_data;
+        src2_is_imm,
+        mem_we,
+        res_from_mem,
+        dest,
+        gr_we} = to_EX_data;
+
+assign to_MEM_data = {alu_result, //32
+                      rkd_value, //32
+                      mem_we, //1
+                      res_from_mem//1
+                      dest, //32
+                      gr_we //1
+                    };
 
 assign alu_src1 = src1_is_pc  ? pc[31:0] : rj_value;
 assign alu_src2 = src2_is_imm ? imm : rkd_value;
@@ -393,5 +396,99 @@ alu u_alu(
     .alu_src2   (alu_src2  ),
     .alu_result (alu_result)
 );
+
+endmodule
+
+module MEM_stage(
+    input   wire                          clk,
+    input   wire                          reset,
+
+    output wire        data_sram_en,
+    output wire [3:0]  data_sram_we, 
+    output wire [31:0] data_sram_addr,
+    output wire [31:0] data_sram_wdata,
+    input  wire [31:0] data_sram_rdata,
+
+    input   wire                          WB_allow_in,
+    input   wire [to_MEM_data_width-1:0]  to_MEM_data,
+    output  wire [to_WB_data_width-1:0]   to_WB_data,
+    output  wire                          MEM_to_WB_valid,
+    output  wire                          MEM_allow_in
+)
+
+reg MEM_valid;
+wire MEM_ready_go;
+
+wire [31:0] alu_result;
+wire [31:0] rkd_value;
+wire        mem_we;
+wire        res_from_mem;
+
+assign MEM_ready_go = 1'b1;//无阻塞
+assign MEM_allow_in = ~MEM_valid | (MEM_ready_go & WB_allow_in);
+assign MEM_to_WB_valid = MEM_valid & MEM_ready_go;
+
+always @(posedge clk) begin
+    if (reset)
+        MEM_valid <= 1'b0;
+    else if (MEM_ready_go)
+        MEM_valid <= EX_to_MEM_valid;
+end
+
+assign {alu_result,
+        rkd_value,
+        mem_we,
+        res_from_mem,
+        dest,
+        gr_we} = to_MEM_data;
+
+assign to_WB_data = {dest, //32
+                     final_result, //32
+                     gr_we //1
+                    };                    
+
+assign data_sram_en    = 1'b1;
+assign data_sram_we    = {4{mem_we && valid}};
+assign data_sram_addr  = alu_result;
+assign data_sram_wdata = rkd_value;
+
+assign mem_result   = data_sram_rdata;
+assign final_result = res_from_mem ? mem_result : alu_result;
+
+endmodule
+
+module WB_stage(
+    input   wire                          clk,
+    input   wire                          reset,
+
+    input   wire [to_WB_data_width-1:0]   to_WB_data,
+    input   wire                          MEM_to_WB_valid,
+    output  wire                          WB_allow_in,
+)
+
+reg WB_valid;
+wire WB_ready_go;
+
+wire [31:0] dest;
+wire [31:0] final_result;
+wire        gr_we;
+
+assgin WB_ready_go = 1'b1;//无阻塞
+assign WB_allow_in = ~WB_valid | WB_ready_go;
+
+always @(posedge clk) begin
+    if (reset)
+        WB_valid <= 1'b0;
+    else if (WB_ready_go)
+        WB_valid <= MEM_to_WB_valid;
+end
+
+assign {dest,
+        final_result,
+        gr_we} = to_WB_data;
+
+assign rf_we    = gr_we && WB_valid;
+assign rf_waddr = dest;
+assign rf_wdata = final_result;
 
 endmodule
